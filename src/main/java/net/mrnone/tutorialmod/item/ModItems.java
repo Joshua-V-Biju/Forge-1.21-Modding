f394c725bcace4a68c28de6c300a6f25f64e29db
JVB Studios <mrnone595@gmail.com>
2026-08-29 16:20:21 +0530

    - Added storm_shard and voidheart_core - Set up Creative Inventory

---
+++ src/main/java/net/mrnone/tutorialmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.mrnone.tutorialmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.mrnone.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> VOIDHEART_CORE = ITEMS.register("voidheart_core", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STORM_SHARD = ITEMS.register("storm_shard", () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+
+
+    }
+
+}
