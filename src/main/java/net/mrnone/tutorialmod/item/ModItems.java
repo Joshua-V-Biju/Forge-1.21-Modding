fcd7f990ed304fdc024275366e4a5825aa5f691d
JVB Studios <mrnone595@gmail.com>
2026-09-12 02:18:17 +0530

    - Added custom tooltips

--- src/main/java/net/mrnone/tutorialmod/item/ModItems.java
+++ src/main/java/net/mrnone/tutorialmod/item/ModItems.java
@@ -1,6 +1,9 @@
 package net.mrnone.tutorialmod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -8,6 +11,8 @@ import net.minecraftforge.registries.RegistryObject;
 import net.mrnone.tutorialmod.TutorialMod;
 import net.mrnone.tutorialmod.item.custom.ChiselItem;
 import net.mrnone.tutorialmod.item.custom.FuelItem;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -23,7 +28,14 @@ public class ModItems {
 
     public static final RegistryObject<Item> STORMDUST_DONUT = ITEMS.register("stormdust_donut", () -> new Item(new Item.Properties().food(ModFoodProperties.STORMDUST_DONUT)));
 
-    public static final RegistryObject<Item> NOVA_CORE = ITEMS.register("nova_core", () -> new FuelItem(new Item.Properties(),30000));
+    public static final RegistryObject<Item> NOVA_CORE = ITEMS.register("nova_core", () -> new FuelItem(new Item.Properties(),30000){
+        @Override
+        public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+            pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.nova_core"));
+
+            super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+        }
+    });
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
